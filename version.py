014e2ab0103fefc1ee1a97b0f14d7388861d0ac6
Code Carefully <CodeCarefully@users.noreply.github.com>
2021-09-23 18:52:46 +0300

    added versioning system

---
+++ version.py
@@ -0,0 +1,29 @@
+__version_info__ = {
+    'major': 0,
+    'minor': 1,
+    'micro': 0,
+    'releaselevel': 'alpha',
+    'serial': 1
+}
+
+import git
+
+
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.commit.hexsha
+        short_sha = repo.git.rev_parse(sha, short=7)
+        vers.append(" GIT:"+short_sha)
+    return ''.join(vers)
+
+__version__ = get_version()
+
+
+if __name__ == '__main__':
+    print(__version__ )
