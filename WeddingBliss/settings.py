6e625091deac1ba1d7a2ec985458488c7a5f94c5
Code Carefully <CodeCarefully@users.noreply.github.com>
2021-09-27 15:43:12 +0300

    added whitenoise for static file sharing. after much struggling, i was forced to make two static file dirs for static files to work, and i have no idea why. i can't add the "root" static directory to the STATICFILES_DIRS, but if i don't it won't find the files.

--- WeddingBliss/settings.py
+++ WeddingBliss/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'WeddingBliss.urls'
@@ -137,9 +138,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATIC_URL = 'http://static.wedding.com/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
