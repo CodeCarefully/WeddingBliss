76fe0b86ed55fbbd7029242f129d5fa6d2908f03
Code Carefully <CodeCarefully@users.noreply.github.com>
2021-09-29 01:28:46 +0300

    started to lay the base for multiple weddings

--- invite/models.py
+++ invite/models.py
@@ -4,18 +4,22 @@ from django.db import models
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django.conf import settings
+import re
+from django.template.defaultfilters import slugify
 
 # https://docs.djangoproject.com/en/3.2/ref/models/fields/
 
 class Weddingevent(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = models.SlugField(blank=True)
     person1 = models.CharField(max_length=300)
     person2 = models.CharField(max_length=300)
     greeting = models.TextField(blank=True)
     blurb = models.TextField(blank=True)
     body = models.TextField(blank=True)
     location = models.TextField(blank=True)
-    meetingURL = models.CharField(max_length=1000)
+    meetingURL = models.CharField(max_length=1000,blank=True)
+    autotriggerdomain = models.CharField(max_length=1000, blank=True)
     time_and_date = models.DateTimeField()
     notepad = models.TextField(blank=True)
     template_name = models.CharField(max_length=800,default="default")
@@ -24,6 +28,11 @@ class Weddingevent(TimeStampedModel):
         null=True,
         on_delete=models.SET_NULL
     )
+
+    def save(self, **kwargs):
+        slug_str = f"{ self.person1 } { self.person2 }"
+        unique_slugify(self, slug_str, slug_separator='_')
+        super(Weddingevent, self).save(**kwargs)
 
     def __str__(self):
         toret = f"Wedding of { self.person1 } and { self.person2 } at { self.time_and_date }"
@@ -112,3 +121,73 @@ class Guest(models.Model):
         return 'Guest: {} {}'.format(self.first_name, self.last_name)
 
 
+#unique slugify from https://djangosnippets.org/snippets/690/
+#author https://djangosnippets.org/users/SmileyChris/
+def unique_slugify(instance, value, slug_field_name='slug', queryset=None,
+                   slug_separator='-'):
+    """
+    Calculates and stores a unique slug of ``value`` for an instance.
+
+    ``slug_field_name`` should be a string matching the name of the field to
+    store the slug in (and the field to check against for uniqueness).
+
+    ``queryset`` usually doesn't need to be explicitly provided - it'll default
+    to using the ``.all()`` queryset from the model's default manager.
+    """
+    slug_field = instance._meta.get_field(slug_field_name)
+
+    slug = getattr(instance, slug_field.attname)
+    slug_len = slug_field.max_length
+
+    # Sort out the initial slug, limiting its length if necessary.
+    slug = slugify(value)
+    if slug_len:
+        slug = slug[:slug_len]
+    slug = _slug_strip(slug, slug_separator)
+    original_slug = slug
+
+    # Create the queryset if one wasn't explicitly provided and exclude the
+    # current instance from the queryset.
+    if queryset is None:
+        queryset = instance.__class__._default_manager.all()
+    if instance.pk:
+        queryset = queryset.exclude(pk=instance.pk)
+
+    # Find a unique slug. If one matches, at '-2' to the end and try again
+    # (then '-3', etc).
+    next = 2
+    while not slug or queryset.filter(**{slug_field_name: slug}):
+        slug = original_slug
+        end = '%s%s' % (slug_separator, next)
+        if slug_len and len(slug) + len(end) > slug_len:
+            slug = slug[:slug_len-len(end)]
+            slug = _slug_strip(slug, slug_separator)
+        slug = '%s%s' % (slug, end)
+        next += 1
+
+    setattr(instance, slug_field.attname, slug)
+
+
+def _slug_strip(value, separator='-'):
+    """
+    Cleans up a slug by removing slug separator characters that occur at the
+    beginning or end of a slug.
+
+    If an alternate separator is used, it will also replace any instances of
+    the default '-' separator with the new separator.
+    """
+    separator = separator or ''
+    if separator == '-' or not separator:
+        re_sep = '-'
+    else:
+        re_sep = '(?:-|%s)' % re.escape(separator)
+    # Remove multiple instances and if an alternate separator is provided,
+    # replace the default '-' separator.
+    if separator != re_sep:
+        value = re.sub('%s+' % re_sep, separator, value)
+    # Remove separator from the beginning and end of the slug.
+    if separator:
+        if separator != '-':
+            re_sep = re.escape(separator)
+        value = re.sub(r'^%s+|%s+$' % (re_sep, re_sep), '', value)
+    return value
