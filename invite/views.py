ce80034f7480fc931cb4b2c35233098fd4fb8c23
Code Carefully <CodeCarefully@users.noreply.github.com>
2021-10-04 02:44:39 +0300

    switched to class based views, and implemented the "wedding chooser" initial view

--- invite/views.py
+++ invite/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import TemplateView
+from invite.models import Weddingevent
 
 
-def index(request):
-    template = loader.get_template('default/index.html')
-    context = {
-        'latest_question_list': '',
-    }
-    return HttpResponse(template.render(context, request))
+class HomepageView(TemplateView):
+    model = Weddingevent
+    template_name = 'weddingchooser.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['weddings'] = [(obj.slug, obj.person1, obj.person2) for obj in Weddingevent.objects.all()]
+        return context
+
